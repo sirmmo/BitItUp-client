23d5b69fa78fff995fcb69c4fe00dec0d392e8aa
Marco Montanari <marco.montanari@gmail.com>
2013-07-14 14:46:46 +0200

    init

---
+++ BackItUp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace BackItUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Panel2.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(490, 244);
+            this.splitContainer1.SplitterDistance = 163;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.FullRowSelect = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(163, 244);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(323, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(323, 220);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(315, 194);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Settings";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(315, 194);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Directory for backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 244);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ BackItUp/Form1.cs
@@ -0,0 +1,149 @@
+﻿using MonoTorrent;
+using MonoTorrent.BEncoding;
+using MonoTorrent.Client;
+using MonoTorrent.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackItUp
+{
+    public partial class Form1 : Form
+    {
+
+        private ClientEngine engine;
+        private Dictionary<DirectoryInfo, FileSystemWatcher> watchers = new Dictionary<DirectoryInfo, FileSystemWatcher>();
+        private Dictionary<DirectoryInfo, Torrent> torrents = new Dictionary<DirectoryInfo, Torrent>();
+        private Dictionary<DirectoryInfo, TorrentManager> mgrs = new Dictionary<DirectoryInfo, TorrentManager>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListDirectory(treeView1, "C:/");
+            EngineSettings es = new EngineSettings();
+            es.AllowedEncryption = MonoTorrent.Client.Encryption.EncryptionTypes.All;
+            es.PreferEncryption = true;
+            engine = new ClientEngine(es);
+            engine.StartAll();
+            
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            TreeNode directoryNode = new TreeNode(rootDirectoryInfo.Name);
+            directoryNode.Tag = rootDirectoryInfo;
+            treeView.Nodes.Add(directoryNode);
+            CreateDirectoryNode(directoryNode);
+        }
+
+        private void ExpandDirectory(TreeNode node)
+        {
+            var directoryInfo = node.Tag as DirectoryInfo;
+            node.Nodes.Clear();
+            CreateDirectoryNode(node);
+        }
+
+        private static void CreateDirectoryNode(TreeNode directoryNode)
+        {
+            var directoryInfo = directoryNode.Tag as DirectoryInfo;
+            try
+            {
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    var idn = new TreeNode(directory.Name);
+                    idn.Tag = directory;
+                    directoryNode.Nodes.Add(idn);
+                }
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    directoryNode.Nodes.Add(new TreeNode(file.Name));
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            TreeNode t = e.Node;
+            ExpandDirectory(t);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            DirectoryInfo di = e.Node.Tag as DirectoryInfo;
+            if (di != null)
+            {
+                if (e.Node.Checked)
+                {
+                    if (!(watchers.ContainsKey(di)))
+                    {
+                        watchers.Add(di, new FileSystemWatcher(di.FullName));
+                        watchers[di].BeginInit();
+                        CreateTorrent(di);
+
+                    }
+                }
+                else
+                {
+                    if (watchers.ContainsKey(di))
+                    {
+                        watchers.Remove(di);
+                        var t = torrents[di];
+                        torrents.Remove(di);
+                    }
+                }
+            }
+        }
+
+        private void CreateTorrent(DirectoryInfo path)
+        {
+            TorrentCreator c = new TorrentCreator();
+
+            RawTrackerTier tier = new RawTrackerTier();
+            tier.Add("http://localhost/announce");
+            c.Announces.Add(tier);
+            c.Comment = "This is the comment";
+            c.CreatedBy = "Doug using " + VersionInfo.ClientVersion;
+            c.Publisher = "www.aaronsen.com";
+
+            c.Private = true;
+
+            c.Hashed += delegate(object o, TorrentCreatorEventArgs e)
+            {
+                Console.WriteLine("Current File is {0}% hashed", e.FileCompletion);
+                Console.WriteLine("Overall {0}% hashed", e.OverallCompletion);
+                Console.WriteLine("Total data to hash: {0}", e.OverallSize);
+            };
+
+            ITorrentFileSource fileSource = new TorrentFileSource(path.FullName);
+            torrents.Add(path, Torrent.Load(c.Create(fileSource)));
+        }
+
+        private void LoadTorrent(String url) {
+            Torrent torrent = Torrent.Load(new Uri(url),"tmp");
+            TorrentManager m = new TorrentManager(torrent, "DOWN", new TorrentSettings());
+            engine.StartAll();
+        }
+
+    }
+
+
+}
